0ec422a3f453bc50f51f97278dbd41e2882c3ba9
superourss <citizen.fx.exoty@gmail.com>
2024-07-20 07:45:39 +0200

    Added copyright footer and updated styles

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function acheter(produit, prix) {
-    const url = `https://paypal.me/superours120/${prix}`;
-    window.location.href = url;
-}
+// Assurez-vous d'avoir inclus confetti.min.js dans votre fichier HTML
+
+document.addEventListener('DOMContentLoaded', function() {
+    const logo = document.querySelector('.logo');
+
+    logo.addEventListener('mouseover', function() {
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 },
+            colors: ['#009FFF', '#0E4E8E']
+        });
+    });
+});
